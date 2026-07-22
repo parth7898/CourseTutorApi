33cde32b2a36b1a562b22b7c2cdc89732885f4f0
parth <parthnew3@gmail.com>
2026-07-22 15:02:48 +0530

    Implement optimized course listing API

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::CoursesController < ApplicationController
   def create
     course = CourseCreationService.new(course_params).call
 
-    render json: course.as_json(include: :tutors), status: :created
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    render json: course, status: :created
+  end
+
+  def index
+    courses = Course.includes(:tutors)
+
+    render json: courses, status: :ok
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  private
+
+  def render_unprocessable_entity(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
